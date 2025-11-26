b25cb5aac1656bc13953edd9d018e7bcaa942984
Niklas Arlt <niklas.arlt@kit.edu>
2025-11-26 09:50:21 +0100

    Manual workspace selector and sshfs

--- init.lua
+++ init.lua
@@ -337,8 +337,22 @@ require("lazy").setup({
             vim.cmd.colorscheme("vscode_modern")
         end,
     },
+    {
+        "uhs-robert/sshfs.nvim",
+        opts = {
+            -- Refer to the configuration section below
+            -- or leave empty for defaults
+        },
+    },
 
-
+    {
+        "nosduco/remote-sshfs.nvim",
+        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+        opts = {
+            -- Refer to the configuration section below
+            -- or leave empty for defaults
+        },
+    }
 
 
   -- Sessions
@@ -358,6 +372,60 @@ require("lazy").setup({
   -- Project management
   --{ "ahmedkhalf/project.nvim" },
 })
+
+-- ===============
+-- SSHFS Setup
+-- ===============
+require("sshfs").setup({
+  -- Your configuration here
+})
+
+require('remote-sshfs').setup{
+  connections = {
+    ssh_configs = { -- which ssh configs to parse for hosts list
+      vim.fn.expand "$HOME" .. "/.ssh/config",
+      "/etc/ssh/ssh_config",
+      -- "/path/to/custom/ssh_config"
+    },
+    ssh_known_hosts = vim.fn.expand "$HOME" .. "/.ssh/known_hosts",
+    -- NOTE: Can define ssh_configs similarly to include all configs in a folder
+    -- ssh_configs = vim.split(vim.fn.globpath(vim.fn.expand "$HOME" .. "/.ssh/configs", "*"), "\n")
+    sshfs_args = { -- arguments to pass to the sshfs command
+      "-o reconnect",
+      "-o ConnectTimeout=5",
+    },
+  },
+  mounts = {
+    base_dir = vim.fn.expand "$HOME" .. "/.sshfs/", -- base directory for mount points
+    unmount_on_exit = true, -- run sshfs as foreground, will unmount on vim exit
+  },
+  handlers = {
+    on_connect = {
+      change_dir = true, -- when connected change vim working directory to mount point
+    },
+    on_disconnect = {
+      clean_mount_folders = false, -- remove mount point folder on disconnect/unmount
+    },
+    on_edit = {}, -- not yet implemented
+  },
+  ui = {
+    select_prompts = false, -- not yet implemented
+    confirm = {
+      connect = true, -- prompt y/n when host is selected to connect to
+      change_dir = false, -- prompt y/n to change working directory on connection (only applicable if handlers.on_connect.change_dir is enabled)
+    },
+  },
+  log = {
+    enabled = false, -- enable logging
+    truncate = false, -- truncate logs
+    types = { -- enabled log types
+      all = false,
+      util = false,
+      handler = false,
+      sshfs = false,
+    },
+  },
+}
 
 -- ================
 -- LSP (clangd)
@@ -389,6 +457,7 @@ vim.lsp.config('pyright', {
     on_init = function(client)
         client.config.settings.python.pythonPath = python_path
     end,
+    on_new_config = python_integration.on_new_config,
     settings = {
         python = {
             pythonpath = python_path,
@@ -647,3 +716,14 @@ vim.api.nvim_create_autocmd("VimEnter", {
     end
   end,
 })
+
+vim.api.nvim_create_user_command("SetWorkspaceRoot", function()
+    local root = find_workspace_root()
+    if root and root ~= vim.fn.getcwd() then
+      vim.cmd("cd " .. vim.fn.fnameescape(root))
+      print("Workspace root: " .. root)
+    else
+      print("Workspace root is already set to: " .. root)
+    end
+  print("Workspace root: " .. find_workspace_root())
+end, { desc = "Set workspace root" })
