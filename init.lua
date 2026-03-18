13129fb26ea22931af4e53bb6f82fddd17a1b0b2
Niklas Arlt <niklas.arlt@kit.edu>
2026-03-18 12:45:55 +0100

    Markdown rendering improvements

--- init.lua
+++ init.lua
@@ -504,6 +504,16 @@ require("lazy").setup({
             require("mini.bufremove").setup()
         end,
     }
+    ,
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+        ---@module 'render-markdown'
+        ---@type render.md.UserConfig
+        opts = {},
+    }
 
 
 
@@ -593,6 +603,21 @@ require("telescope").setup({
       },
     },
   },
+})
+
+-- ==================
+-- Markdown rendering
+-- ==================
+require('render-markdown').setup({
+    completions = { lsp = { enabled = true } },
+    latex = { enabled = true },
+    bullet = {
+        -- Turn on / off list bullet rendering
+        enabled = true,
+        icons = { '▪', '▫', '▸', '▹' },
+        --icons = { '▸', '▹', '▪', '▫' },
+        --icons = { '●', '○', '◆', '◇' },
+    },
 })
 
 
